f79924fd30ec0a63eccedf17c2bcdcbb6cb5a8d9
Nick <nick@monksoftware.co.uk>
2015-11-08 23:30:15 +0000

    Storing timestamped prices in market history

--- src/cryptocoin/market_history.clj
+++ src/cryptocoin/market_history.clj
@@ -1,16 +1,34 @@
-(ns cryptocoin.market-history)
+(ns cryptocoin.market-history
+  "Manage the history of the maket for analysis.
+  - The market-history atom defines a history of prices for each currency pair in any update provided by update-markets
+  - market-history holds a vector of timestamped prices for each currency pair with the most recent price at the head
+    e.g. {...
+          :BTC_XMR [{:timestamp 123123123, :price 0.001312},
+                   {:timestamp 112121212, :price 0.001423},
+                   ...]
+          ...")
 
 (def market-history (atom {}))
 
 (defn last-price-changed? [currency-pair market-value]
-  (let [history-price (-> @market-history currency-pair :last)
+  (let [history-price (-> @market-history currency-pair first :price)
         price         (:last market-value)]
     (not= history-price price)))
+
+(defn prepend-history [current-market-value history]
+  (let [cons-vec (fnil cons [])
+        change   {:timestamp (System/currentTimeMillis)
+                  :price     (:last current-market-value)}]
+    (cons-vec change history)))
 
 (defn update-markets [current-markets]
   (doseq [[currency-pair current-market-value] current-markets]
     (when (last-price-changed? currency-pair current-market-value)
       (println currency-pair
-               "price changed, from:" (-> @market-history currency-pair :last)
+               "price changed, from:" (-> @market-history currency-pair first :price)
                ", to: " (:last current-market-value))
-      (swap! market-history assoc-in [currency-pair :last] (:last current-market-value)))))
+
+      (swap! market-history update-in [currency-pair] #(prepend-history current-market-value %)))))
+
+
+;; TODO: Hold last 1000 prices in history for each currency pair
